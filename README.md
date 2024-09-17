d9ef5b77862233548f1fb28f8f64de8453876697
Martin Dittrich <6719169+emdiet@users.noreply.github.com>
2024-09-17 09:08:18 -0400

    flesh out readme, added init --import

--- README.md
+++ README.md
@@ -1,2 +1,65 @@
 # plp
 plp: python lone projects. pip, but more like npm
+
+## What is this?
+
+This is a pip wrapper that keeps all dependencies in your project direcory, and manages virtual enviroments behind the scenes.
+
+It's like npm(lite), but for python.
+
+
+## Usage
+
+1) have python/pip installed
+2) copy plp.py into your project directory
+3) inside your project directory run `python ./plp.py init`
+4) install packages like you would with pip: `python ./plp.py install <packages, args, etc.>`
+5) run your python scripts with `python ./plp.py run <script-name.py>`
+
+e.g.: 
+
+```powershell
+python ./plp.py init
+python ./plp.py install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu124
+python ./plp.py run my_inference_script.py
+```
+
+ just works. (in windows). no need to activate virtual environments, no need to worry about global packages, no need to worry about system packages.
+
+## Motivation
+
+I wanted to install pytorch cuda (https://pytorch.org/get-started/locally/), but 
+1) installing it with pip is a bad idea, according to everyone
+2) I'm afraid of forgetting to activate/deactivate virtual environments
+3) the poetry maintainers are jerks (https://github.com/python-poetry/poetry/issues/7685)
+4) activating virtual environments on windows doesn't actually work unless you set the execution policy to unrestricted (https://stackoverflow.com/questions/18713086/virtualenv-wont-activate-on-windows). How about... no?
+
+So that's how bob's your uncle.
+
+## How it works
+
+1) `init` creates a virtual environment called `.venv` (your "node_modules") and a `plp.json` (your "package.json") file in the current directory
+2) `install` installs the package into the `.venv` virtual environment, and adds it to the `plp.json` file, and uses the standard `requirements.txt` as your lock file, meaning your pip friends or your docker build or whatever can just use that if they don't want to use plp.
+3) `run` ensures that the script is run in the `.venv` virtual environment. 
+
+## Notes on: requirements.txt
+
+`install` will honor the requirements.txt file if it exists, even if the dependencies are not in plp.py. plp will still properly install everything in the appropriate virtual environment, so you can use plp with your existing projects.
+
+## Notes on: plp.json
+
+`plp.json` is where your core dependencies could live, if you wanted to reinstall the project. It's like a lightweight version of the `package.json` file. You can ignore it if you want, and use the requirements.txt to port your dependencies. It might be useful however if you want to clean your dependencies at some point.
+
+## Notes on: Using this collabortively with git:
+
+add `.venv` to your `.gitignore` file
+
+your buddies can just work with the requirements.txt file if they don't want to use plp.
+
+## Dependencies
+
+- python
+
+## Feature Requests
+
+please open an issue, thx :)
--- plp.py
+++ plp.py
@@ -21,34 +21,102 @@ parser = argparse.ArgumentParser(prog='plp', description='A pip wrapper acting l
 subparsers = parser.add_subparsers(dest='command')
 
 init_parser = subparsers.add_parser('init', help='Initialize a new plp project')
+init_parser.add_argument('--import', dest="import_requirements_from_requirements", action='store_true', help='import requirements from requirements.txt into plp.json. Only necessary if you want to delete or clean requirements.txt')
 
-def plp_init():
+def plp_init(args):
     project_file = 'plp.json'
-    if os.path.exists(project_file):
+    requirements_file = 'requirements.txt'
+    venv_dir = os.path.join(os.getcwd(), '.venv')
+
+    # if project file and virtual environment already exist, do nothing
+    if os.path.exists(project_file) and os.path.exists(venv_dir):
         print('Project already initialized.')
         return
+    
+    # virtual environment setup
+    if not os.path.exists(venv_dir):
+        # Create virtual environment
+        venv.create(venv_dir, with_pip=True)
+        print('Initialized empty plp virtual environment.')
+    else:
+        print('Virtual environment already exists. Skipping creation.')
+    
+    # project file setup
+    if not os.path.exists(project_file):
+        project_data = {
+            'name': os.path.basename(os.getcwd()),
+            'dependencies': {}
+        }
+        with open(project_file, 'w') as f:
+            json.dump(project_data, f, indent=4)
+        print('Initialized empty plp.json.')
+    else:
+        print('plp.json found. skipping creation.')
+    
+    # requirements file setup
+    if not os.path.exists(requirements_file):
+        if args.import_requirements_from_requirements:
+            # print warning message
+            print('warning: --import flag is set but requirements.txt not found. Ignoring flag.')
+
+        with open(requirements_file, 'w') as f:
+            f.write('# this is your requirements lock file\n')
+            print('Initialized empty requirements.txt.')
+    else:
+        if args.import_requirements_from_requirements:
+            # print notice
+            print('Importing requirements from requirements.txt into plp.json.')
+            with open(requirements_file, 'r') as f:
+                lines = f.readlines()
+                for line in lines:
+                    if not line.startswith('#'):
+                        project_data['dependencies'][line.strip()] = line.strip()
+            # merge dependencies, favoring plp.json
+            with open(project_file, 'w') as f:
+                json.dump(project_data, f, indent=4)
+            print('Imported requirements from requirements.txt into plp.json.')
+        else:
+            print('requirements.txt found. Use `plp install` to install dependencies.')
+    
+
+
+def init_from_requirements(requirements_file, write_to_plp_dependencies=False):
+    project_file = 'plp.json'
     project_data = {
         'name': os.path.basename(os.getcwd()),
-        'dependencies': {}
+        'dependencies': [],
+        'comment': "Note: core dependencies could not be determined from requirements.txt. 'plp install' still works though.",
+        'from_requirements_no_core_dependencies': True
     }
+
+    # Read dependencies from requirements.txt
+    if write_to_plp_dependencies:
+        with open(requirements_file, 'r') as f:
+            lines = f.readlines()
+            for line in lines:
+                if not line.startswith('#'):
+                    project_data['dependencies'].append(line.strip())
+        
+    # Create plp.json with dependencies from requirements.txt
     with open(project_file, 'w') as f:
         json.dump(project_data, f, indent=4)
-    # Create virtual environment
-    venv_dir = os.path.join(os.getcwd(), '.venv')
-    venv.create(venv_dir, with_pip=True)
-    print('Initialized empty plp project with virtual environment.')
+    print('Initialized plp project from requirements.txt.')
 
 install_parser = subparsers.add_parser('install', help='Install packages')
 install_parser.add_argument('packages', nargs='*', help='Packages to install')
 install_parser.add_argument('--global', dest='global_install', action='store_true', help='Install globally')
 install_parser.add_argument('pip_args', nargs=argparse.REMAINDER, help='Additional arguments for pip')
 
-def plp_install(packages, global_install, pip_args):
+def plp_install(packages, global_install, pip_args, project_file=None, save=True, update_lock=True):
     # Ensure project is initialized
-    project_file = find_project_file()
+    project_file = find_project_file() if not project_file else project_file
     if not project_file:
-        print('Error: No plp project found. Run plp init first.')
-        sys.exit(1)
+        # check if requirements.txt exists and initialize from it
+        if os.path.exists('requirements.txt'):
+            plp_init({'skip_confirmation': False})
+        else:
+            print('Error: No plp project found. Run plp init first.')
+            sys.exit(1)
 
     # Load project data
     with open(project_file, 'r') as f:
@@ -58,9 +126,18 @@ def plp_install(packages, global_install, pip_args):
     local_packages_dir = os.path.join(os.path.dirname(project_file), 'local_packages')
 
     if not packages:
-        # Install all dependencies from plp.json
-        packages = list(project_data['dependencies'].values())
+        # install all dependencies from requirements.txt
+        if os.path.exists('requirements.txt'):
+            with open('requirements.txt', 'r') as f:
+                locked_packages = [line.strip() for line in f if line.strip() and not line.startswith('#')]
+                plp_install(locked_packages, False, pip_args, project_file, save=False, update_lock=False)
 
+        # Install all additional dependencies from plp.json, pip will ignore already installed packages
+        desired_packages = list(project_data['dependencies'].values())
+        plp_install(desired_packages, False, pip_args, project_file, save=False, update_lock=True)
+        return
+
+    # Install packages globally
     if global_install:
         subprocess.run([sys.executable, '-m', 'pip', 'install'] + packages + pip_args)
         return
@@ -76,13 +153,23 @@ def plp_install(packages, global_install, pip_args):
     subprocess.run(pip_args)
 
     # Update project dependencies if new packages were provided
-    for package in packages:
-        package_name = package.split('==')[0]
-        project_data['dependencies'][package_name] = package
+    if save:
+        for package in packages:
+            package_name = package.split('==')[0]
+            project_data['dependencies'][package_name] = package
 
     # Save updated project data
     with open(project_file, 'w') as f:
         json.dump(project_data, f, indent=4)
+
+    # Update requirements.txt
+    # Capture the current state of installed packages using pip freeze
+    result = subprocess.run([pip_executable, 'freeze'], capture_output=True, text=True)
+    if result.returncode != 0:
+        print('Error: Unable to update requirements.txt.')
+        return
+    with open('requirements.txt', 'w') as f:
+        f.write(result.stdout)
 
 
 def find_project_file():
@@ -130,7 +217,7 @@ def plp_run(script, script_args):
 def main():
     args = parser.parse_args()
     if args.command == 'init':
-        plp_init()
+        plp_init(args)
     elif args.command == 'install':
         plp_install(args.packages, args.global_install, args.pip_args)
     elif args.command == 'run':
